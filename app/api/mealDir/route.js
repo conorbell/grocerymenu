d6af9354d741fd77e3d3d6683f91cc557140ea1c
conorbell <conorbell27@gmail.com>
2024-02-24 15:48:43 -0500

    removed await db connection

--- app/api/mealDir/route.js
+++ app/api/mealDir/route.js
@@ -3,7 +3,6 @@ import { MealDirectory } from '@/models/schema';
 import { NextResponse } from 'next/server';
 
 export const GET = async (request) => {
-  await connectMongoDB();
   const meals = await MealDirectory.find();
   // console.log('meals', meals);
   return NextResponse.json({ meals: meals });
