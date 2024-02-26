b46a99da63135bbb0a35befbbdd3986205020c02
conorbell <conorbell27@gmail.com>
2024-02-26 14:33:35 -0500

    added better error handling to meal directory

--- app/api/mealDir/route.js
+++ app/api/mealDir/route.js
@@ -3,14 +3,27 @@ import { MealDirectory } from '@/models/schema';
 import { NextResponse } from 'next/server';
 
 export const GET = async (request) => {
-  await connectMongoDB();
-  const meals = await MealDirectory.find();
-  return NextResponse.json({ meals });
+  try {
+    await connectMongoDB();
+    const meals = await MealDirectory.find();
+    return NextResponse.json({ meals });
+  } catch (error) {
+    console.error('Error fetching meals:', error);
+    return NextResponse.json(
+      { error: 'Error fetching meals' },
+      { status: 500 }
+    );
+  }
 };
 
 export const POST = async (request) => {
-  await connectMongoDB();
-  const { title, category, img, ingredients } = await request.json();
-  await MealDirectory.create({ title, category, img, ingredients });
-  return NextResponse.json({ message: 'Meal created' }, { status: 201 });
+  try {
+    await connectMongoDB();
+    const { title, category, img, ingredients } = await request.json();
+    await MealDirectory.create({ title, category, img, ingredients });
+    return NextResponse.json({ message: 'Meal created' }, { status: 201 });
+  } catch (error) {
+    console.error('Error creating meal:', error);
+    return NextResponse.json({ error: 'Error creating meal' }, { status: 500 });
+  }
 };
