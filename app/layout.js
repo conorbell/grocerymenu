b5d231a51b48688df77d39699ac89cc8df673498
conorbell <conorbell27@gmail.com>
2024-03-03 17:24:44 -0500

    updated title

--- app/layout.js
+++ app/layout.js
@@ -7,8 +7,8 @@ import { ListProvider } from '@/app/Context/store.jsx';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Paragon Honda',
-  description: 'Number 1 Honda Dearlership In The World',
+  title: "Chef's Selection",
+  description: 'Create a grocery list from the ingredients in your go-to meals',
 };
 
 export default async function RootLayout({ children }) {
--- app/page.js
+++ app/page.js
@@ -2,16 +2,16 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="">
-      <div className="grid place-content-center mt-16 ">
-        <h1 className="mb-20">Paragon Honda #1 Honda Dealer In The World</h1>
+    <main className=''>
+      <div className='grid place-content-center mt-16 '>
+        <h1 className='mb-20 text-[2em]'>Chef's Selection</h1>
         <Image
-          className="mt-10"
-          loading="lazy"
+          className='mt-10'
+          loading='lazy'
           width={500}
           height={500}
-          alt="red car"
-          src="/static/images/hondar.avif"
+          alt='red car'
+          src='/static/images/chefs.png'
         />
       </div>
     </main>
